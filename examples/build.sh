ac61c8caa8de25c24929633290e108120fc42fc3
Anton Pozharskiy <apozharski@gmail.com>
2025-08-25 22:38:04 +0200

    bulid script

---
+++ examples/build.sh
@@ -0,0 +1,2 @@
+gcc basic_problem.c -B ../build/ -I ../build/ -I ~/.julia/juliaup/julia-1.12-nightly/include/julia/ -L/home/anton/syscop/software/libMad/build/ -lMad
+LD_LIBRARY_PATH="/home/anton/syscop/software/libMad/build/" ./a.out 
